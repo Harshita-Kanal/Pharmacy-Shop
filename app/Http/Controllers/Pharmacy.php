e9f3f40d89a092ddcfc7af9b71fc9ea571238953
Harshita-Kanal <harshita.kgv@gmail.com>
2020-12-02 21:11:36 +0530

    Added initial files

---
+++ app/Http/Controllers/Pharmacy.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Pharmacy extends Controller
+{
+    //
+    function index(){
+        return "Welcome to the shop!";
+    }
+}
---
+++ app/Http/Controllers/pharmacontroller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class pharmacontroller extends Controller
+{
+    //
+    function index(){
+        return "Welcome to the shop!";
+    }
+}
